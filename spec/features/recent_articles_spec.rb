092afe961dfdfdaf5bb528a31d2a7c57864abd4b
Dhruv Raj <rajdhruv30@gmail.com>
2019-09-19 02:04:07 +0530

    test-2 : all data filtered by status are arranged in order of date

--- spec/features/recent_articles_spec.rb
+++ spec/features/recent_articles_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
 RSpec.feature "Widget management", :type => :feature do
+	before(:each) do
+		@text_articles = Article.order("updated_at desc")
+    	@text_articles = @text_articles.where("created_at >= ?", Time.now - 7.days)
+	end
   scenario "Correctly displays Articles" do
     visit "/articles"
     expect(page).to have_selector 'h1', text: 'Recent Articles'
-    @text_articles = Article.order("updated_at desc")
-    @text_articles = @text_articles.where("created_at >= ?", Time.now - 7.days)
     @text_articles=@text_articles[0..19]
     expected_title = @text_articles.map(&:title)
 	actual_title = all('td.title').map(&:text)
@@ -13,6 +15,14 @@ RSpec.feature "Widget management", :type => :feature do
   end
 
   scenario "Correctly filters Articles by publishing status" do
-    visit "/articles"
+    visit "/articles?status=published"
+    @text_articles = @text_articles.where("published_at IS NOT NULL AND published_at < ?", Time.now)
+    @text_articles=@text_articles[0..19]
+    expected_title = @text_articles.map(&:title)
+	actual_title = all('td.title').map(&:text)
+	all_displayed_dates = all('td.published-at').map(&:text)
+	expected_sorted_dates = all_displayed_dates.sort
+	expect(actual_title).to eq(expected_title)
+	expect(all_displayed_dates).to eq(expected_sorted_dates)
   end
 end
