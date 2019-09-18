97a8c1ad3ea58b5874c284860b862b835ab9d585
Dhruv Raj <rajdhruv30@gmail.com>
2019-09-19 03:09:24 +0530

    test-4: Added Uniqueness of email at DB and Model Level and performed Test Case for Uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :articles
+  validates_uniqueness_of :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:valid_attributes) {{ name: "Test User", email: "testuser2@mailid.com"}}
+ 	let(:user) { User.new(valid_attributes) }
+ 	before do
+      User.create(valid_attributes)
+    end
+    it "requires a unique email" do
+      expect(user).to validate_uniqueness_of(:email)
+    end
 end
