77b4ed8bd7d90ad25fbadb28033c9f7cd80e614a
Dhruv Raj <rajdhruv30@gmail.com>
2019-09-19 03:56:08 +0530

    test-7: Reduced Code Content on View Using Helper Method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def article_status(article)
+		return (article.published_at.nil? || article.published_at > Time.now) ? "draft" : "published" 
+	end
+	def article_published_date(article)
+		return article.published_at.strftime("%Y-%m-%d") if article.published_at 
+	end
+	def authors_name(articles)
+		return articles.collect {|article| article.author.name }.uniq.join(", ")
+	end
 end
